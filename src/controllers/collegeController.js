780039cc4d61160845de71f0189606e71a97cc00
Saswath1403 <saswathsingh1403@gmail.com>
2022-06-28 21:19:04 +0530

    1st Api done!

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -11,19 +11,40 @@ const objectValue = function (value) {
 }
 
 const createCollege = async function (req, res) {
+    try {
+        const { name, fullName, logoLink } = req.body
 
-    const { name, fullName, logoLink } = req.body
+        if (Object.keys(req.body).length === 0) return res.status(400).send({ status: false, msg: "All fields are empty!" })
 
-    if (!objectValue(name)) return res.status(400).send({ status: false, msg: "Name is required!" })
-    if (!objectValue(fullName)) return res.status(400).send({ status: false, msg: "fullName is required!" })
-    if (!objectValue(logoLink)) return res.status(400).send({ status: false, msg: "logoLink is required!" })
+        if (!objectValue(name)) return res.status(400).send({ status: false, msg: "name is required!" })
 
+        let nameRegex = /^[A-Za-z\s]{1,}[\]{0,1}[A-Za-z\s]{0,}$/;
+
+        if (!nameRegex.test(name)) return res.status(400).send({ status: false, msg: "name must be in characters!" })
+
+        const duplicateName = await collegeModel.findOne({ name })
+
+        if (duplicateName) return res.status(400).send({ status: false, msg: "This college name is already used!" })
+
+        let collegeRegex = /^[A-Za-z\s]{1,}[\.,'-]{0,1}[A-Za-z\s]{0,}$/;
+
+        if (!collegeRegex.test(fullName)) return res.status(400).send({ status: false, msg: "College full name must be in characters!" })
+
+        if (!objectValue(fullName)) return res.status(400).send({ status: false, msg: "fullName is required!" })
+
+        if (!objectValue(logoLink)) return res.status(400).send({ status: false, msg: "logoLink is required!" })
+
+        const college = await collegeModel.create(req.body)
+
+        res.status(201).send({ status: true, data: college })
+    }
+    catch (error) {
+        res.status(500).send({ status: false, msg: error.message })
+    }
 
 }
 
 
-
-
-
+module.exports = { createCollege }
 
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,6 @@ const route = express.Router();
 const collegeController = require("../controllers/collegeController");
 const internController = require("../controllers/internController");
 
-
+route.post("/functionup/colleges" , collegeController.createCollege)
 
 module.exports = route
