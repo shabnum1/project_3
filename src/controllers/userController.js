81982b681a747bf6da318e14bc9831168d050342
Saswath1403 <saswathsingh1403@gmail.com>
2022-07-06 13:53:54 +0530

    Project in progress

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
-const collegeModel = require("../models/collegeModel");
-const axios = require('axios')
+const userModel = require("../models/userModel");
+
 const { urlRegex, objectValue, nameRegex, collegeRegex, keyValue } = require("../middleware/validator"); // IMPORTING VALIDATORS
 
 
@@ -7,66 +7,24 @@ const { urlRegex, objectValue, nameRegex, collegeRegex, keyValue } = require("..
 
 // V = Validator 
 
-const createCollege = async (req, res) => {
+const createUser = async (req, res) => {
     try {
+        
+     let {title, name, email, phone, password, address } = req.body
 
-        res.setHeader('Access-Control-Allow-Origin','*')
+     let street = req.body.address.street
 
-        let nameInLowerCase = req.body.name   
+     let city = req.body.address.city
 
-        if (!keyValue(req.body)) return res.status(400).send({ status: false, msg: "All fields are empty!" })  // 3rd V used here
+     let pincode = req.body.address.pincode
 
-        if (!objectValue(nameInLowerCase)) return res.status(400).send({ status: false, msg: "name is required!" })  // 2nd V used here
+     if (!keyValue(req.body)) return res.status(400).send({status: false, msg: "Please provide details!"})
 
-        nameInLowerCase = nameInLowerCase.toLowerCase()   // input in Lower case
+     if (title || title === "") {
+        if (!objectValue(title)) return res.status(400).send({status: false, msg: "Please enter title!"})
+     }
 
-        const { fullName, logoLink, isDeleted } = req.body    // Destructuring
-
-        if (!nameRegex(nameInLowerCase)) return res.status(400).send({ status: false, msg: "name must be in alphabet and atleast of 2 characters!" })   // 4th V used here
        
-        const duplicateName = await collegeModel.findOne({name: nameInLowerCase })
-
-        if (duplicateName) return res.status(400).send({ status: false, msg: "This college name is already used!" }) 
-
-        if (!objectValue(fullName)) return res.status(400).send({ status: false, msg: "fullName is required!" })  // 2nd V used here
-
-        if (!collegeRegex(fullName)) return res.status(400).send({ status: false, msg: "College full name must be in characters and of atleast 5 characters long!" }) // 5th V used here
-
-        if (!objectValue(logoLink)) return res.status(400).send({ status: false, msg: "logoLink is required!" })  // 2nd V used here
-
-        if (!urlRegex(logoLink)) return res.status(400).send({ status: false, msg: "logoLink is invalid!" })  // 8th V used here
-
-        let duplicateLogoLink = await collegeModel.findOne({logoLink:logoLink})
-
-        if(duplicateLogoLink) return res.status(400).send ({status: false , msg: "logo link already in use!"})
-  
-      
-        let validLogolink = false
-        await axios.get(logoLink)
-            .then((url) => {
-                if (url.status === 200 || 201) {                              
-                    if (url.headers["content-type"].startsWith("image/"))    // AXIOS VALIDATION
-                        validLogolink = true;
-                }
-            })
-            .catch((error) => validLogolink = false)
-
-        if (validLogolink === false) return res.status(404).send({ status: false, msg: "either logo link is incorrect or does not exist!" })
-
-        if (isDeleted === "") {         // Nested 'if' statment used here
-            if (!objectValue(isDeleted)) return res.status(400).send({ status: false, msg: "isDeleted is invalid!" })  // 2nd V used here
-        }
-
-        if (isDeleted && typeof isDeleted !== "boolean") return res.status(400).send({ status: false, msg: "isDeleted should be either true or false!" })
-
-        const collegeData = {name:nameInLowerCase, fullName, logoLink, isDeleted}
-
-        const collegeCreation = await collegeModel.create(collegeData)
-
-        const college = await collegeModel.findOne({name:nameInLowerCase}).select({ _id: 0, __v: 0 })
-
-
-        res.status(201).send({ status: true, data:college})
     }
     catch (error) {
         res.status(500).send({ status: false, msg: error.message })
