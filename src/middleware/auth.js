5defa5bc300fe099218e6ebbd6e575eae8dee3b4
Saswath1403 <saswathsingh1403@gmail.com>
2022-07-13 01:16:25 +0530

    done

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");    // Importing
 
-//======================================================1st Middleware===================================================================//
+//====================================================Authentication Middleware=============================================================//
 
 const authentication = async function (req, res, next) {                  // Authentication
     try {
@@ -16,36 +16,6 @@ const authentication = async function (req, res, next) {                  // Aut
         res.status(500).send({ status: false, error: err.message })
 
     }
-
 }
 
-//========================================================2nd Middleware===================================================================//
-
-// const authorization = async function (req, res, next) {                         // Authorization
-
-//     try {
-
-//         let token = req.headers["x-api-key"]
-
-//         if (!token) return res.status(400).send({ status: false, msg: "No Token Found!" })
-
-//         let decodedToken = jwt.verify(token, "group66-project3")
-
-//         if (!decodedToken) return res.status(401).send({ status: false, msg: "Invalid token!" })
-
-//         let usersId = decodedToken.userId
-//         let bodyData = req.body.userId
-
-//         console.log(bodyData)
-
-//         if (usersId != bodyData) return res.status(403).send({ status: false, msg: "Not Authorized!" })
-
-//         next()
-//     }
-//     catch (err) {
-//         res.status(500).send({ status: false, Error: err.message })
-//     }
-// }
-
-
-module.exports = { authentication }     // Exporting them
+module.exports = { authentication }     // Exporting 
