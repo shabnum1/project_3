7d9945cda51cb0cb2f0da751249beec43602cf13
Saswath1403 <saswathsingh1403@gmail.com>
2022-07-13 11:27:50 +0530

    done

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,27 +2,25 @@ const jwt = require("jsonwebtoken");    // Importing
 
 //====================================================Authentication Middleware=============================================================//
 
-const authentication = async function (req, res, next) {                  // Authentication
-    try {
-        let token = req.headers["x-api-key"]
-
-        if (!token) return res.status(400).send({ status: false, msg: "No Token Found!" })
-        let decodedToken = jwt.verify(token, "group66-project3", (err, decoded) => {
-            if (err) {
-              return res.status(400).send({ status: false, Error: err.message })
-               
-            } else {
-                return decoded
-            }
-        })
-        if (!decodedToken) return res.status(401).send({ status: false, msg: "Invalid token!" })
-
-        next()
-
-    } catch (err) {
-        res.status(500).send({ status: false, error: err.message })
-
-    }
+const authentication= async function(req,res,next){
+    try{
+     let token= req.headers["X-Api-key"];
+     if(!token) token= req.headers["x-api-key"]
+     if (!token) return res.send({ status: false, message: "token must be present" }); 
+     jwt.verify(token, "group66-project3",function (err, decoded) {
+        if (err) {
+             return res.status(401).send({ status: false, message: "invalid token" })
+        } else {
+            console.log(decoded)
+            req.decodedToken=decoded
+            next()
+        }
+    })
+    
+}
+catch(err){
+    return res.status(500).send({status:false,message:err.message})
+}
 }
 
 module.exports = { authentication }     // Exporting 
